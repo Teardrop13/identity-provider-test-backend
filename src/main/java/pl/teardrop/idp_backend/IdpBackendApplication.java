65ef6b5ec24fa8fbcee1c287b91a1bfe7bec94eb
Michał Lewandowski <michal.lewandowski99@gmail.com>
2024-08-02 20:34:49 +0200

    init commit

---
+++ src/main/java/pl/teardrop/idp_backend/IdpBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.teardrop.idp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdpBackendApplication.class, args);
+	}
+
+}
